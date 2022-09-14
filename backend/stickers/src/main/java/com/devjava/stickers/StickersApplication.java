457b9f0d34f8570a63a1d6647b6fde207e41f2d5
frankkol <frankkol@hotmail.com>
2022-09-13 21:16:24 -0300

    Project created

---
+++ backend/stickers/src/main/java/com/devjava/stickers/StickersApplication.java
@@ -0,0 +1,13 @@
+package com.devjava.stickers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StickersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StickersApplication.class, args);
+	}
+
+}
---
+++ backend/stickers/src/test/java/com/devjava/stickers/StickersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjava.stickers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StickersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
